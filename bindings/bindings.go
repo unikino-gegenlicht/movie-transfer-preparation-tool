2c1cea0836d487afbf9c8232d8513e5f24296d2c
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2023-03-17 11:32:32 +0100

    :art: adding new data binding for configuration

--- bindings/bindings.go
+++ bindings/bindings.go
@@ -1,8 +1,12 @@
 package bindings
 
-import "fyne.io/fyne/v2/data/binding"
+import (
+	"fyne.io/fyne/v2/data/binding"
+	"movie-transfer-preparation-tool/structs"
+)
 
 var CurrentStartupStep = binding.NewString()
 var ExternalDrives = binding.NewUntyped()
 var SelectedDrive = binding.NewString()
 var Movies = binding.NewUntypedList()
+var Configuration = binding.BindUntyped(structs.Configuration{})
