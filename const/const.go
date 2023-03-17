46888c7de109824a473a542d2c04b8810659b117
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2023-03-17 10:14:27 +0100

    :ambulance: fixed error with date time validator

--- const/const.go
+++ const/const.go
@@ -3,4 +3,4 @@ package consts
 const DateFormat = "02.01.2006"
 const TimeFormat = "15:04"
 const DateTimeFolderFormat = "2006-01-02 1504"
-const DateTimeFormat = "2006-01-02 15:04"
+const DateTimeFormat = "02.01.2006 15:04"
