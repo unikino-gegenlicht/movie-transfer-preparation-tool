5cbe9f069a767d02262bca9bb2af2c7d7d0405cb
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2023-03-17 11:25:50 +0100

    :construction: adding binding to keep selected drive

--- bindings/bindings.go
+++ bindings/bindings.go
@@ -4,4 +4,5 @@ import "fyne.io/fyne/v2/data/binding"
 
 var CurrentStartupStep = binding.NewString()
 var ExternalDrives = binding.NewUntyped()
+var SelectedDrive = binding.NewString()
 var Movies = binding.NewUntypedList()
